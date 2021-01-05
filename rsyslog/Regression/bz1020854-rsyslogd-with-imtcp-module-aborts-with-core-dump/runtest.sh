57650d59bdf81bc9f6214eb9f237697db3b44b0d
Dalibor Pospisil <dapospis@redhat.com>
2021-01-05 01:18:47 +0100

    updated test library reference with inherited fmf id

--- rsyslog/Regression/bz1020854-rsyslogd-with-imtcp-module-aborts-with-core-dump/runtest.sh
+++ rsyslog/Regression/bz1020854-rsyslogd-with-imtcp-module-aborts-with-core-dump/runtest.sh
@@ -27,7 +27,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Include Beaker environment
-. /usr/bin/rhts-environment.sh || :
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="rsyslog"
--- sudo/Regression/bz469653-sudo-l-segfaults-when-executed-in-ElectricFence/runtest.sh
+++ sudo/Regression/bz469653-sudo-l-segfaults-when-executed-in-ElectricFence/runtest.sh
@@ -28,8 +28,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Include rhts environment
-. /usr/bin/rhts-environment.sh || :
-. /usr/lib/beakerlib/beakerlib.sh
+. /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="sudo"
 _LIB_EFENCE_F=""
