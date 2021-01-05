34a8849a3a19797317fcd0ab0800ded073882307
Dalibor Pospisil <dapospis@redhat.com>
2021-01-05 19:56:30 +0100

    sudo/Regression/bz1672876-Backporting...: updated metadata

--- sudo/Regression/bz1672876-Backporting-sudo-bug-with-expired-passwords/runtest.sh
+++ sudo/Regression/bz1672876-Backporting-sudo-bug-with-expired-passwords/runtest.sh
@@ -26,7 +26,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Include Beaker environment
-. /usr/bin/rhts-environment.sh || :
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 PACKAGE="sudo"
