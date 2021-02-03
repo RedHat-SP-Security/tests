1e8802c935392035a9cd9e4ccc96115a1c1b089f
Ondrej Moris <omoris@redhat.com>
2021-02-03 09:26:41 +0100

    libcap-ng: rhel-8.4 fix and debugging improvement
    Signed-off-by: Ondrej Moris <omoris@redhat.com>

--- libcap-ng/Regression/compare-capabilities-from-captest-and-capsh/runtest.sh
+++ libcap-ng/Regression/compare-capabilities-from-captest-and-capsh/runtest.sh
@@ -34,22 +34,24 @@ PACKAGE="libcap-ng"
 rlJournalStart
     rlPhaseStartSetup
         rlAssertRpm $PACKAGE
-	rlRun "uname -a"
+	      rlRun "uname -a"
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
-	rlRun "captest --text &> captest.out"
-	rlRun "captest &> hexadec.out"
-	HEXA=`awk '/^Effective:/ { print $2,$3; exit }' hexadec.out | sed 's/, //'`
-	rlLogInfo "hexadecimal representation of Effective capabilities: $HEXA"
-	rlRun "capsh --decode=$HEXA &> capsh.out"
-	# now convert output to a better form
-	rlRun "cut -d '=' -f 2 capsh.out | sed -e 's/cap_//g' -e 's/,/\\n/g' | sort > capsh_sorted.out"
-	rlRun "sed -e 's/35/wake_alarm/' -e 's/36/block_suspend/' -e 's/37/audit_read/' capsh_sorted.out | sort > capsh_sorted2.out" 0 "substituting unknown/numeric capabilities in the output"
-	rlRun "grep '^Effective' captest.out | sed -e 's/Effective: //' -e 's/, /\\n/g' | sort > captest_sorted.out"
+	      rlRun -s "captest --text"
+        cp $rlRun_LOG captest.out
+	      rlRun -s "captest"
+        cp $rlRun_LOG  hexadec.out
+	      HEXA=`awk '/^Effective:/ { print $2,$3; exit }' hexadec.out | sed 's/, //'`
+	      rlLogInfo "hexadecimal representation of Effective capabilities: $HEXA"
+	      rlRun "capsh --decode=$HEXA | tee capsh.out"
+	      # now convert output to a better form
+	      rlRun "cut -d '=' -f 2 capsh.out | sed -e 's/cap_//g' -e 's/,/\\n/g' | sort > capsh_sorted.out"
+	      rlRun "sed -e 's/35/wake_alarm/' -e 's/36/block_suspend/' -e 's/37/audit_read/' -e 's/38/cap_38/' -e 's/39/cap_39/' capsh_sorted.out | sort > capsh_sorted2.out" 0 "substituting unknown/numeric capabilities in the output"
+	      rlRun "grep '^Effective' captest.out | sed -e 's/Effective: //' -e 's/, /\\n/g' | sort > captest_sorted.out"
     rlPhaseEnd
 
     rlPhaseStartTest
-	rlRun "diff capsh_sorted2.out captest_sorted.out" 0 "'Effective permissions listed by captest --text' and 'capsh --decode=$HEXA' should match"
+	      rlRun "diff capsh_sorted2.out captest_sorted.out" 0 "'Effective permissions listed by captest --text' and 'capsh --decode=$HEXA' should match"
     rlPhaseEnd
 
     rlPhaseStartCleanup
