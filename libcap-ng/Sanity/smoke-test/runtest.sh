3de94d002784f272ef0dde2485a97e8893cdbb55
Martin Zeleny <mzeleny@redhat.com>
2021-06-02 17:05:21 +0200

    libcap-ng/Sanity/smoke-test
    Fixing test for new rebase.

--- libcap-ng/Sanity/smoke-test/runtest.sh
+++ libcap-ng/Sanity/smoke-test/runtest.sh
@@ -33,63 +33,52 @@
 PACKAGE="libcap-ng"
 
 TESTUSER="libcapngtestuser"
+SPECFILE="~${TESTUSER}/rpmbuild/SPECS/libcap-ng.spec"
 
 rlJournalStart
 
     rlPhaseStartSetup
-    
-        rlAssertRpm $PACKAGE	
-	rlRun "useradd $TESTUSER" 0-255
-	rlFetchSrcForInstalled $PACKAGE
-
+        rlAssertRpm $PACKAGE
+        rlRun "useradd $TESTUSER" 0-255
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+        rlFetchSrcForInstalled $PACKAGE
+        rlRun "chown -R ${TESTUSER}:${TESTUSER} ${TmpDir}"
+        rlRun "SRC_RPM=$(ls libcap-ng*.src.rpm)"
     rlPhaseEnd
 
-    rlPhaseStartTest
-    
-         rlRun "cp *.rpm /tmp/libcap.src.rpm" 0
+    rlPhaseStartTest "Rebuilding ${SRC_RPM}"
+        rlRun "/sbin/runuser -s /bin/sh -c 'rpm -ihv ${SRC_RPM}' -- ${TESTUSER}" 0 "Installing libcap-ng source RPM"
+        rlRun "dnf builddep ${SPECFILE} -y"
 
-	 rlRun "/sbin/runuser -s /bin/sh -c \
-                'rpm -ihv /tmp/libcap.src.rpm' -- $TESTUSER" 0 \
-	     "Installing libcap-ng source RPM"
-	 
-         rlRun "/sbin/runuser -s /bin/sh -c \
-                'rpmbuild -vv -bc ~/rpmbuild/SPECS/libcap-ng.spec' \
-                -- $TESTUSER" 0 \
-	     "Building libcap-ng source RPM"
+        rlRun "/sbin/runuser -s /bin/sh -c 'rpmbuild -vv -bc ${SPECFILE}' -- ${TESTUSER}" 0 "Building libcap-ng source RPM"
 
-         rlRun "/sbin/runuser -s /bin/sh -c \
-                \"make -C ~/rpmbuild/BUILD/libcap-ng* check\" \
-                >/tmp/make.check.out -- $TESTUSER" 0 \
-	     "Running libcap-ng self-test (as non-root user)"
+        rlRun "/sbin/runuser -s /bin/sh -c \"make -C ~/rpmbuild/BUILD/libcap-ng* check\" > make.check.out -- ${TESTUSER}" 0 "Running libcap-ng self-test (as non-root user)"
+        rlRun "cat make.check.out"
 
-	 
-	 cat /tmp/make.check.out
+        if rlIsRHEL 6; then
+            rlAssertNotGrep "failed"  make.check.out
+            rlAssertNotGrep "skipped" make.check.out
+            rlRun "[ `grep passed make.check.out | wc -l` -eq 2 ]" 0 "All tests should pass."
+        else
+            rlAssertEquals "1st set of tests should pass" \
+            `grep '# PASS:' make.check.out | awk 'NR==1 {print $3}'` \
+            `grep '# TOTAL:' make.check.out | awk 'NR==1 {print $3}'`
 
-	if rlIsRHEL 6; then
-            rlAssertNotGrep "failed"  /tmp/make.check.out
-            rlAssertNotGrep "skipped" /tmp/make.check.out
-            rlRun "[ `grep passed /tmp/make.check.out | wc -l` -eq 2 ]" 0 "All tests should pass."
-	else
-	    rlAssertEquals "1st set of tests should pass" \
-                       `grep '# PASS:' /tmp/make.check.out | awk 'NR==1 {print $3}'` \
-                       `grep '# TOTAL:' /tmp/make.check.out | awk 'NR==1 {print $3}'`
             if rlIsRHEL "<8"; then
-	        rlAssertEquals "2nd set of tests should pass" \
-                               `grep '# PASS:' /tmp/make.check.out | awk 'NR==2 {print $3}'` \
-                               `grep '# TOTAL:' /tmp/make.check.out | awk 'NR==2 {print $3}'`
+                rlAssertEquals "2nd set of tests should pass" \
+                `grep '# PASS:' make.check.out | awk 'NR==2 {print $3}'` \
+                `grep '# TOTAL:' make.check.out | awk 'NR==2 {print $3}'`
             fi
-	fi
-	 rm -f /tmp/make.check.out
-	 
+        fi
+        rm -f make.check.out
     rlPhaseEnd
 
     rlPhaseStartCleanup
-
-	rlRun "userdel $TESTUSER" 0
-	rlRun "rm -rf /home/$TESTUSER" 0
-
+        rlRun "userdel --remove ${TESTUSER}" 0
+        rlRun "popd"
+        rlRun "rm -r ${TmpDir}" 0 "Removing tmp directory"
     rlPhaseEnd
 
 rlJournalPrintText
-
 rlJournalEnd
