2cd4c2b8e3b7d6bee978b3d2f78f4d5334d117a7
Dalibor Pospisil <dapospis@redhat.com>
2022-01-31 20:28:51 +0100

    fapolicyd/Sanity/cli-options: fixed a search for libc.so

--- fapolicyd/Sanity/cli-options/runtest.sh
+++ fapolicyd/Sanity/cli-options/runtest.sh
@@ -161,7 +161,7 @@ rlJournalStart && {
       rlRun "cp '$(rpm -qal | grep '\.pyc$' | head -n 1)' files/application_x-bytecode.python_.pyc"
       rlRun "cp '$(rpm -qal | grep '\.so$' | grep -v libc | grep -v pthread | head -n 1)' files/application_x-sharedlib_non-libc.so"
       rlRun "cp /bin/bash files/application_x-executable_bash"
-      libc=$(rpm -ql glibc | grep -E 'libc.*\.so(\.[0-9]+)?$' | head -n1)
+      libc=$(rpm -ql glibc | grep -E '^/lib(64)?/libc\b.*\.so(\.[0-9]+)?$' | head -n1)
       tcfChk "checking $libc for application/x-sharedlib" && {
         echo -n "file's output:      "; file --mime $libc
         echo -n "fapolicyd's output: "; fapolicyd-cli -t $libc | tee out
