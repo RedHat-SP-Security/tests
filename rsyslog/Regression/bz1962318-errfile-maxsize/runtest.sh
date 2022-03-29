347fa454edf13b64a36e9c465cf2de72e9ba1f5d
Sergio Arroutbi <sarroutb@redhat.com>
2022-03-29 20:12:11 +0200

    Check action.errorfile.maxsize works appropriately (#89)
    * Check action.errorfile.maxsize works appropriately
    
    action.errorfile.maxsize configuration allows to limit
    the error file to a particular size. This test checks
    that error file is limited to that size.
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>
    Co-authored-by: Dalibor Pospisil <dapospis@redhat.com>

---
+++ rsyslog/Regression/bz1962318-errfile-maxsize/runtest.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /rsyslog/Regression/bz1962318-errfile-maxsize
+#   Author: Sergio Arroutbi <sarroutb@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2022 Red Hat, Inc.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/bin/rhts-environment.sh || :
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+PACKAGE="rsyslog"
+RSYSLOG_ERRFILE=$(mktemp)
+RSYSLOG_ERRFILE_MAXSIZE=1234
+NUM_LOGS=200
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm $PACKAGE
+        rlRun "rlImport --all" || rlDie "cannot continue"
+        rlRun "rsyslogSetup"
+        rlLog "Updating /etc/rsyslog.conf"
+        rsyslogConfigAppend "RULES" <<EOF
+action(type="omfwd" target="1.2.3.4" port="1234" Protocol="tcp" NetworkNamespace="doesNotExist"
+       action.errorfile="${RSYSLOG_ERRFILE}" action.errorfile.maxsize="${RSYSLOG_ERRFILE_MAXSIZE}")
+EOF
+        rlRun "rsyslogPrintEffectiveConfig -n"
+        rlRun "rsyslogServiceStart"
+        sleep 3
+        rlRun "rsyslogServiceStatus"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Ensure initially empty error file size limits to ${RSYSLOG_ERRFILE_MAXSIZE}"
+        for (( counter=0; counter<${NUM_LOGS}; counter++ ))
+        do
+             logger "Message:[${counter}]"
+        done
+        rlAssertExists ${RSYSLOG_ERRFILE}
+        size=$(ls -l ${RSYSLOG_ERRFILE} | awk {'print $5'})
+        rlAssertEquals "Checking initally empty error file:${RSYSLOG_ERRFILE} has size:${RSYSLOG_ERRFILE_MAXSIZE}" "${size}" "${RSYSLOG_ERRFILE_MAXSIZE}"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Ensure non initially empty error file size limits to ${RSYSLOG_ERRFILE_MAXSIZE}"
+        rlServiceStop rsyslog
+        # Dump some info to error file and check after rebooting it also does not write more than expected
+        dd if=/dev/urandom of=${RSYSLOG_ERRFILE} bs=1 count=$((RSYSLOG_ERRFILE_MAXSIZE-100))
+        rlServiceStart rsyslog
+        for (( counter=0; counter<${NUM_LOGS}; counter++ ))
+        do
+             logger "Message:[${counter}]"
+        done
+        rlAssertExists ${RSYSLOG_ERRFILE}
+        size=$(ls -l ${RSYSLOG_ERRFILE} | awk {'print $5'})
+        rlAssertEquals "Checking not initally empty error file:${RSYSLOG_ERRFILE} has size:${RSYSLOG_ERRFILE_MAXSIZE}" "${size}" "${RSYSLOG_ERRFILE_MAXSIZE}"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rm -fvr ${RSYSLOG_ERRFILE}
+        rlFileRestore
+        rlRun "rsyslogCleanup"
+    rlPhaseEnd
+
+rlJournalPrintText
+rlJournalEnd
