cb50138027baf48459c60a1f44d1dfe53396ecc2
Dalibor Pospisil <dapospis@redhat.com>
2022-06-02 10:57:30 +0200

    rsyslog/Sanity/test-various-configura...: do not use HOSTNAME variable

--- rsyslog/Sanity/test-various-configuration-directives/runtest.sh
+++ rsyslog/Sanity/test-various-configuration-directives/runtest.sh
@@ -152,11 +152,11 @@ EOF
         rlRun "logger -p local0.info 'test message from logger command'" 0 "Logging test using logger command"
         rsyslogServiceStart
         sleep 1
-        cat /var/log/rsyslog-PreserveFQDN-test.log
-        [[ "$HOSTNAME" == "$fqdn" ]]
-        rlAssert0 "Checking that my hostname $HOSTNAME is FQDN" $?
-        rlRun "grep -q '$HOSTNAME' /var/log/rsyslog-PreserveFQDN-test.log" 0 "Checking that FQDN is used in log messages"
-        rlIsRHEL 5 || rlRun "grep -v '$HOSTNAME' /var/log/rsyslog-PreserveFQDN-test.log" 1 "Checking that all messages are using the FQDN"  # bz847967 wontfix on RHEL5
+        rlRun "cat /var/log/rsyslog-PreserveFQDN-test.log" 0-255
+        #[[ "$HOSTNAME" == "$fqdn" ]]
+        #rlAssert0 "Checking that my hostname $HOSTNAME is FQDN" $?
+        rlRun "grep -q '$fqdn' /var/log/rsyslog-PreserveFQDN-test.log" 0 "Checking that FQDN is used in log messages"
+        rlIsRHEL 5 || rlRun "grep -v '$fqdn' /var/log/rsyslog-PreserveFQDN-test.log" 1 "Checking that all messages are using the FQDN"  # bz847967 wontfix on RHEL5
         CleanupDo --mark
       fi
     rlPhaseEnd; }
