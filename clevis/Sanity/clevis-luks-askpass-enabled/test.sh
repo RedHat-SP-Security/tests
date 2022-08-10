198e2a041b29f9703396d45ba4ecb07a434e8995
Martin Zeleny <mzeleny@redhat.com>
2022-08-10 16:43:21 +0200

    clevis/Sanity/clevis-luks-askpass-enabled
    Adding rlAssertRpm

--- clevis/Sanity/clevis-luks-askpass-enabled/test.sh
+++ clevis/Sanity/clevis-luks-askpass-enabled/test.sh
@@ -3,6 +3,12 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm "clevis-systemd"
+        rlIsRHEL && rlAssertRpm "redhat-release"
+        rlIsFedora && rlAssertRpm "fedora-release-common"
+    rlPhaseEnd
+
     rlPhaseStartTest "Check 'systemctl is-enabled clevis-luks-askpass.path' output"
         rlAssertEquals "'systemctl is-enabled clevis-luks-askpass.path' output has to be enabled" \
             "$(systemctl is-enabled clevis-luks-askpass.path)" "enabled"
