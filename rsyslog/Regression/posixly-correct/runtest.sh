c0208a087520480f415ed34ee4819f8693ce216a
Dalibor Pospisil <dapospis@redhat.com>
2022-12-22 11:40:21 +0100

    rsyslog/Regression/posixly-correct: first version

---
+++ rsyslog/Regression/posixly-correct/runtest.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Author: Dalibor Pospisil <dapospis@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2022 Red Hat, Inc.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart && {
+  rlPhaseStartSetup && {
+    rlRun "rlCheckRecommended; rlCheckRequired" || rlDie "cannot continue"
+  rlPhaseEnd; }
+
+  rlPhaseStartTest && {
+    rlAssertGrep 'systemctl -s HUP kill rsyslog' /etc/logrotate.d/syslog
+  rlPhaseEnd; }; :
+
+  rlJournalPrintText
+rlJournalEnd; }
