dbb7bb9c8640171fa677d5b22fa7ce482f6a6046
Martin Zeleny <mzeleny@redhat.com>
2023-06-12 13:53:32 +0200

    clevis/Sanity/password-generation
    Old version used `pwmake` to generate the password for clevis LUKS slot.
    Clevis bind should not fail with pwmake error message about low entropy
    for generating password. `pwmake` is now replaced with `jose`. Verifies
    bz2207488.

--- clevis/Sanity/password-generation/test.sh
+++ clevis/Sanity/password-generation/test.sh
@@ -2,10 +2,12 @@
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
+PACKAGES="clevis clevis-luks tang jose luksmeta cryptsetup"
 PWQ_CONF="/etc/security/pwquality.conf"
 
 rlJournalStart
     rlPhaseStartSetup
+        rlAssertRpm --all
         rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
         rlRun "pushd $tmp"
         rlRun "set -o pipefail"
@@ -20,19 +22,23 @@ rlJournalStart
         rlRun "echo 'maxclassrepeat = 4' > ${PWQ_CONF}"
     rlPhaseEnd
 
-    rlPhaseStartTest
-        counter=1
-        while [ $counter -ne 7 ]; do
-            echo "===  binding:$counter ==="
-            echo -n "redhat123" | clevis luks bind -k - -d ${lodev}  -y tang '{"url":"localhost"}'
-            echo "=== /binding:$counter ==="
-            ((counter++))
+
+    slot_count=6
+    rlPhaseStartTest "${slot_count} attempts for clevis luks bind"
+        for i in $(seq 1 $slot_count); do
+            rlRun "echo -n redhat123 | clevis luks bind -f -k - -d ${lodev} -y tang '{\"url\": \"localhost\"}'" \
+                0 "Clevis binding attempt ${i}/${slot_count}" || break
         done
     rlPhaseEnd
 
+
     rlPhaseStartCleanup
+        rlRun "rlServiceRestore tangd.socket"
+        rlRun "find /dev/mapper -name 'luks*' -exec cryptsetup close {} +"
+        rlRun "losetup -d ${lodev}"
         rlRun "popd"
         rlRun "rm -r $tmp" 0 "Remove tmp directory"
         rlFileRestore
     rlPhaseEnd
+rlJournalPrintText
 rlJournalEnd
