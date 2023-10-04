3aacccbdf9ac6469d3681526b88cedb1a452bf2c
Sergio Arroutbi <sarroutb@redhat.com>
2023-10-04 10:45:49 +0200

    Add malware detection test (#148)
    Resolves: #147
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ tang-operator/Sanity/mount_image.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2023 Red Hat, Inc.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function usage() {
+  echo "$1 -v version [-c container_manager (podman by default)]"
+  exit "$2"
+}
+
+while getopts "c:v:h" arg
+do
+  case "${arg}" in
+    c) CONTAINER_MGR=${OPTARG}
+       ;;
+    v) VERSION=${OPTARG}
+       ;;
+    h) usage "$0" 0
+       ;;
+    *) usage "$0" 1
+       ;;
+  esac
+done
+
+test -z "${CONTAINER_MGR}" && CONTAINER_MGR='podman'
+test -z "${VERSION}" && usage "$0" "1"
+"${CONTAINER_MGR}" image mount "${VERSION}"
